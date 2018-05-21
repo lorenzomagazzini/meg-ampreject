84a4778f7d0e5b334d0b83f4f4a510c71bdf488a
c1356674 <magazzinil@cardiff.ac.uk>
2018-05-21 18:27:43 +0100

    add padding for 1hz highpass filtering and  multitrial plot

--- scripts/multitrialplot_example.m
+++ scripts/multitrialplot_example.m
@@ -23,7 +23,7 @@ nsubj = length(file_list);
 %%
 
 %define subj from list
-s = 3
+s = 5
 subj_label = strrep(file_list{s}, '_data.mat', '');
 
 %load data
@@ -40,12 +40,32 @@ end
 
 %%
 
+% %low-level padding
+% paddata = data;
+% padlength       = 2* ceil( abs(data.time{1}(end)-data.time{1}(1)) ) * data.fsample;
+% prepadlength    = padlength/2;
+% postpadlength   = padlength/2;
+% padtype         = 'mirror';
+% for i = 1:length(data.trial)
+%     dat = ft_preproc_padding(data.trial{i}, padtype, prepadlength, postpadlength);
+%     tim = linspace(data.time{i}(1)-(padlength/data.fsample)/2, data.time{i}(end)+(padlength/data.fsample)/2, size(dat,2));
+%     paddata.trial{i} = dat;
+%     paddata.time{i} = tim;
+% end
+% paddata = rmfield(paddata,'sampleinfo')
+
 %low-pass filtering
 cfg = [];
 cfg.lpfilter = 'yes';
 cfg.lpfreq = 4;
 cfg.padding = 0.5*(abs(data.time{1}(end)-data.time{1}(1)))+abs(data.time{1}(end)-data.time{1}(1));
 data_lp = ft_preprocessing(cfg,data);
+% data_lp = ft_preprocessing(cfg,paddata);
+
+% %trim down
+% cfg = [];
+% cfg.latency = [data.time{1}(1) data.time{1}(end)];
+% data_lp = ft_selectdata(cfg, data_lp)
 
 %interactive multitrial plot
 cfgplot = [];
